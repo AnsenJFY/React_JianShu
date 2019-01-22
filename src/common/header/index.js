045e4d34d703075077febbda8e0c47c312582f51
AnsenJ <ansen2014@foxmail.com>
2019-01-22 18:01:24 +0800

    初始化项目，并使用style-component完成组件的编写，傻逼逼写了半天才发现有个对应vscode插件

---
+++ src/common/header/index.js
@@ -0,0 +1,24 @@
+import React , {Component} from 'react';
+import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button} from './style'
+
+class Header extends Component{
+    render(){
+        return (
+            <HeaderWrapper>
+                <Logo></Logo>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登陆</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavSearch></NavSearch>
+                    <Addition>
+                        <Button className="writing">写文章</Button>
+                        <Button className="reg">注册</Button>
+                    </Addition>
+                </Nav>
+            </HeaderWrapper>
+        )
+    }
+}
+export default Header;
